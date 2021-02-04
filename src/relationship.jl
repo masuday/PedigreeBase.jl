1b8470357ef0b77ec40742ee4c574d5a72dac41f
masuday <masdee@gmail.com>
2021-02-03 21:09:20 -0500

    support for A-inverse ignoring inbreeding

--- src/relationship.jl
+++ src/relationship.jl
@@ -77,8 +77,11 @@ end
 
 """
     Ainv = get_nrminv(pedlist, f; rank=0)
+    Ainv = get_nrminv(pedlist; f, rank=0)
 
-Computes the inverse of numerator (additive) relationship matrix using the "Henderson's" method from the pedigree list `pedlist` and inbreeding coefficients `f`. See Henderson (1976) and Quaas (1976) for details.
+Computes the inverse of numerator (additive) relationship matrix using the "Henderson's" method from the pedigree list `pedlist` and inbreeding coefficients `f`.
+Without `f`, this function assumes zero inbreeding for all individuals.
+See Henderson (1976) and Quaas (1976) for details.
 The output is SparseMatrixCSC with the same type of elements with `f` while SparseMatrixDict (i.e., a hash matrix) is used as a temporary matrix.
 If you want to fix the size of A-inverse, specify `rank`.
 The function creates the inverse with either a bigger size: `rank` or the maximum ID in the pedigree.
@@ -123,9 +126,9 @@ function get_nrminv(pedlist::Matrix{Ti}, f::Vector{Tv}; rank=0) where {Ti<:Integ
    end
    return sparse(AInv)
 end
-function get_nrminv(pedlist::Matrix{Ti}; f::Vector{Tv}=zeros(Tv,0), rank=0) where {Ti<:Integer,Tv<:AbstractFloat}
+function get_nrminv(pedlist::Matrix{Ti}; f::Vector{Tv}=Float64[], rank=0) where {Ti<:Integer,Tv<:AbstractFloat}
    if length(f)==0
-      inb = zeros(Tv)
+      inb = zeros(Float64,maximum(pedlist))
    else
       inb = f
    end
